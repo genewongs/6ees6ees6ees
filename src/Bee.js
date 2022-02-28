2c90fe1a002e9de7b92384e27f7d2816fe0ed849
Gene Wong <gene.h.wong@gmail.com>
2022-02-28 11:19:05 -0800

    Implemented all classes with passing tests in the style of ES6 instantiation pattern

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
-class Bee {
-  // TODO..
-};
+class Bee extends Grub {
+  constructor() {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+}
+
+//notes:
+//calling super will allow you to inherit all properties from Parent (what you are extending)
+//MDN: The super keyword is used to access and call functions on an object's parent
+//The super() function is used to give access to methods and properties of a parent or sibling class.
